144b74bb1b24c0315406a383f66830d961d11b67
User <User@YD03-01>
2019-09-17 17:41:15 +0900

    9173

--- HelloWorld/src/hello/ArraryExample3_1.java
+++ HelloWorld/src/hello/ArraryExample3_1.java
@@ -2,17 +2,35 @@ package hello;
 
 public class ArraryExample3_1 {
 	public static void main(String[] args) {
+
 		int[][] intAry = new int[2][];
 		intAry[0] = new int[3];
 		intAry[1] = new int[4];
-		int sum=0;
-		for(int i=0; i<intAry.length; i++) {
-			sum=0;
-			for(int j=0; j<intAry[i].length; j++) {
-				sum += intAry[i][j]; 
+		int a = 8;
+		for (int i = 0; i < intAry.length; i++) {
+			for (int j = 0; j < intAry[i].length; j++) {
+				intAry[i][j] = a++;
 			}
-			System.out.println("intAry"+"["+i+"]"+sum);	
-	}
-System.out.println();
-	}
+
+		}
+		for (int i = 0; i < intAry.length; i++) {
+			for (int j = 0; j < intAry[i].length; j++) {
+				// System.out.print(ary[i][j] + " ");
+				System.out.printf("%2d", intAry[i][j]);
+			}
+			System.out.println();
+		}
+	
+	
+
+	int sum=0;for(
+	int i = 0;i<intAry.length;i++)
+	{
+		sum = 0;
+		for (int j = 0; j < intAry[i].length; j++) {
+			sum += intAry[i][j];
+		}
+		System.out.println("intAry" + "[" + i + "]" + sum);
+	}System.out.println();
+}
 }
--- HelloWorld/src/hello/ArrayExample3.java
+++ HelloWorld/src/hello/ArrayExample3.java
@@ -1,34 +1,34 @@
 package hello;
 
-public class ArrayExample3 {
+public class ArraryExample3_1 {
 	public static void main(String[] args) {
+
 		int[][] intAry = new int[2][];
 		intAry[0] = new int[3];
 		intAry[1] = new int[4];
 		int a = 8;
-		for(int i=0; i<intAry.length; i++) {
-			for(int j=0; j<intAry[i].length; j++) {
-				intAry[i][j]=a++;
+		for (int i = 0; i < intAry.length; i++) {
+			for (int j = 0; j < intAry[i].length; j++) {
+				intAry[i][j] = a++;
 			}
+
 		}
-		
-		
-		for(int i=0; i<intAry.length; i++) {
-			for(int j=0; j<intAry[i].length; j++) {
+		for (int i = 0; i < intAry.length; i++) {
+			for (int j = 0; j < intAry[i].length; j++) {
+				// System.out.print(ary[i][j] + " ");
 				System.out.printf("%2d", intAry[i][j]);
 			}
 			System.out.println();
 		}
-		int sum=0;
-		for(int i=0; i<intAry.length; i++) {
-			sum=0;
-			for(int j=0; j<intAry[i].length; j++) {
-				sum += intAry[i][j]; 
-			}
-			System.out.println("intAry"+"["+i+"]"+sum);	
-	}
-System.out.println();
-	}
-	
 
+		int sum = 0;
+		for (int i = 0; i < intAry.length; i++) {
+			sum = 0;
+			for (int j = 0; j < intAry[i].length; j++) {
+				sum += intAry[i][j];
+			}
+			System.out.println("intAry" + "[" + i + "]" + sum);
+		}
+		System.out.println();
+	}
 }
