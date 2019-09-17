6499dae43cb391c41a186a38763c52daf9d56e2c
User <User@YD03-01>
2019-09-17 17:51:06 +0900

    9175

--- HelloWorld/src/hello/ArraryExample3_1.java
+++ HelloWorld/src/hello/ArraryExample3_1.java
@@ -1,8 +1,39 @@
 package hello;
 
+import java.util.Scanner;
+
 public class ArraryExample3_1 {
 	public static void main(String[] args) {
-
+		boolean run = true;
+		int studentNum = 0;
+		int[] scores = null;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run){
+			System.out.println("--------------------------------------------");
+			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
+			System.out.println("--------------------------------------------");
+			System.out.print("선택");
+			
+			int selectNo = scanner.nextInt();
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		int[][] intAry = new int[2][];
 		intAry[0] = new int[3];
 		intAry[1] = new int[4];
