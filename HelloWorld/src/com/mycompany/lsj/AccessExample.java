817c611378ec4037afadab63d874537a8ce3e135
User <User@YD03-01>
2019-09-23 09:01:04 +0900

    092100

--- HelloWorld/src/com/mycompany/lsj/AccessExample.java
+++ HelloWorld/src/com/mycompany/lsj/AccessExample.java
@@ -1,8 +1,12 @@
 package com.mycompany.lsj;
 
 public class AccessExample {
+		public static void main(String[] args) {
+			
+		
 	Access1 acc1 = new Access1();
-	acc1
+	acc1.field1 = 10;
+	
 	Access2 acc2 = new Access2();
-
+		}
 }
