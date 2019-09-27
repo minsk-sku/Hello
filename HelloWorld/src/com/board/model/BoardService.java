5f167ee9b1fef4f1c9ac041564ac532e7281862b
User <User@YD03-01>
2019-09-27 15:40:22 +0900

    092703

--- HelloWorld/src/com/board/model/BoardService.java
+++ HelloWorld/src/com/board/model/BoardService.java
@@ -1,26 +1,14 @@
 package com.board.model;
 
 public interface BoardService {
-	public void writeBoard(Board board, Board[] boards);//글작성
+	public void writeBoard(Board board, Board[] boards);
 
-	public Board getBoard(int boardNo, Board[] boards);//조회
+	public Board getBoard(int boardNo, Board[] boards);
 
-	public Board[] getBoardList(Board[] boards);//전체리스트
-	
-	
-	
-	
-	//내용변경
-	//글삭제
+	public Board[] getBoardList(Board[] boards);
+
 	public void updateBoard(Board board, Board[] boards);
-	
-	
-	
-	
-	
 
-	void deleteBoard(int boardNo, Board[] boards);
-	
-	
-	
+	public void deleteBoard(int boardNo, Board[] boards);
+
 }
